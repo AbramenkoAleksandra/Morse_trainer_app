01cafe0e06f5a376ec73529f30c01ab2819751f2
AbramenkoAL <AbramenkoAleksandra1T@gmail.com>
2026-02-08 01:04:16 +0300

    learning program add data

---
+++ tests/learning_program_test.py
@@ -0,0 +1,20 @@
+import pytest
+from src.learning_program import LearningProgram
+
+lp = LearningProgram()
+
+
+@pytest.mark.parametrize("level, level_data", lp.levels.items())
+def test_level_data(level, level_data):
+    letters = set(level_data.get("letters",[]))
+    new_letters = set(level_data.get("new_letters",[]))
+    if not letters: return
+    if words:=level_data.get("words",[]):
+        for w in words:
+            assert set(w).issubset(letters) == True
+            assert set(w) & new_letters
+    if phrases:=level_data.get("phrases",[]):
+        letters.add(' ')
+        for ph in phrases:
+            assert set(ph).issubset(letters) == True
+            assert set(ph) & new_letters
